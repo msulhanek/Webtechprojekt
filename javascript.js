79764c667b38b312774541000348a1ceb79d2b03
unknown <m.sulhane@gmail.com>
2020-01-03 15:54:13 +0100

    Ignorácia diakritiky pri vyhľadávaní

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-    divko = document.getElementById("text");
+    divko = document.getElementById("meniny");
 
     setInputFilter(document.getElementById('datum'), function(value) {
         return /^\d*\.?\d*$/.test(value); // Allow digits and '.' only, using a RegExp
@@ -8,28 +8,30 @@ window.onload = function(){
 
 $(document).ready(function(){
     $.getJSON('meniny.json',function(data){
-        text = data.meniny.zaznam;
+        poleMien = data.meniny.zaznam;
     })
 })
 
 
-function show(){
-    var a = findDate();
-    $('#text').html('');
-    $.each(text[a], function(i,data){
+function zobraz(){
+    var a = najdiDatum();
+    $('#ukazDatum').html('');
+    document.getElementById("meno").value = '';
+    $('#meniny').html('');
+    $.each(poleMien[a], function(i,data){
         if(i!="den"){
             console.log(data);
-            $('#text').append(data + "<br>");
+            $('#meniny').append(data + "<br>");
         }
         
     })
 }
 
-function findDate(){
+function najdiDatum(){
     var datum = (druhe+prve).toString();
     //console.log((mesiac+den).toString());
     var pozicia;
-    $.each(text, function(i,data){
+    $.each(poleMien, function(i,data){
         
         if(data.den==datum){
             //console.log(data.den, i);
@@ -40,13 +42,13 @@ function findDate(){
     return pozicia;
 }
 
-function zmena(){
+function zmenaDatumu(){
     x = document.getElementById("datum").value;
     console.log(x.length);
     if(x.search(".")!=-1){
          prve = overenieDna(x.split(".")[0]);
          druhe = overenieMesiaca(x.split(".")[1]);
-         show();
+         zobraz();
     }
 }
 
@@ -72,6 +74,78 @@ function overenieMesiaca(mesiac){
     return 0;
 }
 
+function zmenaMena(){
+    var meno = document.getElementById("meno").value;
+    meno = slugify(meno);
+    //document.getElementById("datum").disabled = true;
+    var datum = najdiMeno(meno);
+    zobrazMeno(datum);
+}
+
+function najdiMeno(meno){
+    var datum = 0;
+    var pole;
+    
+    $.each(poleMien, function(i, data){
+        if(typeof(data.SK)!=='undefined'){
+            pole = (slugify(data.SK)).split(", ");
+            
+            for(i=0;i<pole.length;i++){
+                if(pole[i].toLowerCase()==meno.toLowerCase()){
+                    datum = data.den;
+                }
+            }
+        }
+    })
+    
+    return datum;
+}
+
+function zobrazMeno(datum){
+    if(datum>0){
+        upravenyDatum = datum[2] + datum [3] + "." + datum[0] + datum[1];
+        document.getElementById("datum").value = '';
+        $('#ukazDatum').html('');
+        $('#meniny').html('');
+        $('#ukazDatum').append(upravenyDatum);
+    }
+    else{
+        $('#ukazDatum').html('');
+        $('#meniny').html('');
+        $('#ukazDatum').append("Neplatne meno");
+    }
+}
+
+function slugify (str) {
+    var map = {
+        'a' : 'á|ä|â|Á|Â',
+        'e' : 'é|É',
+        'i' : 'í|Í',
+        'o' : 'ó|ô|Ó|Ô',
+        'u' : 'ú|Ú',
+        'c' : 'č|Č',
+        'd' : 'ď|Ď',
+        'r' : 'ŕ|Ŕ',
+        's' : 'š|Š',
+        't' : 'ť|Ť',
+        'z' : 'ž|Ž',
+        'l' : 'ĺ|ľ|Ĺ|Ľ',
+        'n' : 'ň|Ň'
+    };
+    
+    str = str.toLowerCase();
+    
+    for (var pattern in map) {
+        str = str.replace(new RegExp(map[pattern], 'g'), pattern);
+    };
+
+    return str;
+};
+
+
+
+
+
 function setInputFilter(textbox, inputFilter) {
     ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
       textbox.addEventListener(event, function() {
@@ -88,5 +162,6 @@ function setInputFilter(textbox, inputFilter) {
       });
     });
   }
+
 
   
